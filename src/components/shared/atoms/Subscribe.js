a9f30bdc2ad859170a4b26758a0c13a71dfd4564
sarahvc <sarahvc@qq.com>
2018-09-09 01:48:03 -0400

    add subscribe function

--- src/components/shared/atoms/Subscribe.js
+++ src/components/shared/atoms/Subscribe.js
@@ -1,29 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Field } from 'redux-form';
+import { SubscribeInput } from './SubscribeInput';
+import { subscribe } from '../state/actions/SubscribeActions';
 
 export default class Subscribe extends Component {    
     constructor(props) {
         super(props);
 
-        this.state = {
-            subEmail: ''
-        };
+        this.handleSub = this.handleSub.bind(this);
     }
 
-    handleSub() {
-        this.setState({subEmail: 'sarahvc@qq.com'});
+    handleSub(value) {
+        this.props.subscribe(value);
     }
     render () {
         return (
-            <form className='artx-subscribe-form mx-auto'>
+            <form className='artx-subscribe-form mx-auto' onSubmit={this.handleSub}>
                 <div className="form-group text-center">
                     <label className='artx-type-tw artx-gradient-text mb-4' htmlFor='subscribeEmail'>Subcribe to the ARTX email list to get updates on<br/>
                     Decentralism art auctions and ARTX airdrop!</label>
                     <div className='artx-subscribe-input d-flex justify-content-between mx-auto'>
-                        <input className='py-3 apl-8 border-0 w-100 text-white artx-type-tw' type='email' id='subscribeEmail' placeholder='Email'/>
-                        <button className='text-white font-weight-bold border-0 h-100 py-3 px-4' type="submit" onClick={this.handleSub}>JOIN NOW</button>
+                        <Field 
+                            name='subscribeemail'
+                            component = {SubscribeInput}/>
+                        <button className='text-white font-weight-bold border-0 h-100 py-3 px-4' type="submit">JOIN NOW</button>
                     </div>
                 </div>
             </form>
         );
     }
 }
+
+Subscribe.propTypes = {
+    subscribe: PropTypes.func.isRequired,
+    failed: PropTypes.bool.isRequired,
+    fetching: PropTypes.bool.isRequired,
+    fetched: PropTypes.bool.isRequired,
+    email: PropTypes.string.isRequired
+};
+  
+const mapStateToProps = state => {
+    const { failed, fetching, fetched, email } = state.subscribe;
+
+    return { failed, fetching, fetched, email };
+};
+
+const mapDispatchToProps = dispatch => (bindActionCreators({ subscribe }, dispatch)
+);
+
+const hoc = connect(mapStateToProps, mapDispatchToProps)(subscribe);
+
+// EXPORT COMPONENT
+
+export { hoc as Subscribe };
