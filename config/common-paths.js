c459e7337d7eb63043db953d2330b8e57970e509
sarahvc <sarahvc@qq.com>
2018-09-17 23:04:16 -0400

    refactor for services

--- config/common-paths.js
+++ config/common-paths.js
@@ -5,7 +5,7 @@ module.exports = {
     root: path.resolve(__dirname),
     template: './src/index.html',
     favicon: './src/favicon.ico',
-    api_service_url: 'http://localhost:3030/api/v1',
+    api_service_url: 'http://localhost:3579',
     app_contract_address: '0x4a746397E9666642E48cBC3FB6308Fb6D782C4b4', 
     auction_address: '0x4a746397E9666642E48cBC3FB6308Fb6D78asdfasd', 
     //ropsten
